40de709f47d8b44e4715d9c2fa0183515e34c458
khurshid <Khurshid@qurilo.com>
2024-08-13 11:02:32 +0530

    update in bulklead , uploadmiddleware

--- controllers/Bulklead.js
+++ controllers/Bulklead.js
@@ -3,7 +3,17 @@ const { importLeadsFromExcel } = require("../utils/importLeads");
 
 const uploadFile = async (req, res) => {
   try {
-    const filePath = path.join(__dirname, "../", req.file.path);
+    const filePath = path.resolve(
+      __dirname,
+      "..",
+      "uploads",
+      req.file.filename
+    );
+
+    console.log(__dirname, "kd dirname here");
+
+    console.log(filePath, "kd path here");
+
     await importLeadsFromExcel(filePath);
     res.status(200).json({ msg: "file processed successfully" });
   } catch (error) {
@@ -11,4 +21,4 @@ const uploadFile = async (req, res) => {
   }
 };
 
-module.exports = {uploadFile};
+module.exports = { uploadFile };
